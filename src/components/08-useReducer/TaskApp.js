861ff3643c391b122c87ef7f000b6c90f7b0989d
Ozasnidev <ozasnidev@gmail.com>
2020-11-19 20:33:58 +0100

    03 - Create an interface & initial Task

--- src/components/08-useReducer/TaskApp.js
+++ src/components/08-useReducer/TaskApp.js
@@ -13,21 +13,45 @@ export const TaskApp = () => {
     console.log(taskState);
     return (
         <div>
-            <h1>TaskApp</h1>
+            <h1>TaskApp ({taskState.length})</h1>
             <hr/>
-
-            <ol>
-                <li>Seiya</li>
-                <li>Hyoga</li>
-                <li>Shiryu</li>
-                <li>Shun</li>
-                <li>Ikki</li>
-                <li>Jabu</li>
-                <li>Geki</li>
-                <li>Ban</li>
-                <li>Nachi</li>
-                <li>Ichi</li>
-            </ol>
+            <div className="row">
+                <div className="col-7">
+                    <h3>Task</h3>
+                    <hr/>
+                    <ul className="list-group list-group-flush">
+                        {
+                            taskState.map(task => (
+                                <li
+                                    key={task.id}
+                                    className="list-group-item list-group-item-warning"
+                                >
+                                    <p className="text-center">{task.desc}</p>
+                                    <button className="btn btn-outline-danger">Borrar</button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h3>Agregar Task</h3>
+                    <hr/>
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Task"
+                            autoComplete="off"
+                        />
+                        <button
+                            className="btn btn-outline-success mt-2 btn-block"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
