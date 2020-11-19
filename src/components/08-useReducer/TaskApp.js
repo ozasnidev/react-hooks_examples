9ac316c5c2a371573b077834f448c2db2cefe26b
Ozasnidev <ozasnidev@gmail.com>
2020-11-19 22:38:26 +0100

    04 - Add task behavior with reducer

--- src/components/08-useReducer/TaskApp.js
+++ src/components/08-useReducer/TaskApp.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react'
-import './styles.css'
 import { taskReducer } from './taskReducer';
+import './styles.css';
 
 const initialState = [{
     id: new Date().getTime(),
@@ -9,8 +9,25 @@ const initialState = [{
 }];
 
 export const TaskApp = () => {
-    const [taskState] = useReducer(taskReducer, initialState);
-    console.log(taskState);
+    const [taskState, dispatch] = useReducer(taskReducer, initialState);
+    
+    const handleAddTask = (event) => {
+        event.preventDefault();
+
+        const newTask = {
+            id: new Date().getTime(),
+            desc: 'Aprender React a fondo',
+            done: false
+        };
+
+        const taskAction = {
+            type:'add',
+            payload: newTask
+        };
+
+        dispatch(taskAction);
+    }
+
     return (
         <div>
             <h1>TaskApp ({taskState.length})</h1>
@@ -24,7 +41,7 @@ export const TaskApp = () => {
                             taskState.map(task => (
                                 <li
                                     key={task.id}
-                                    className="list-group-item list-group-item-warning"
+                                    className="list-group-item list-group-item-warning m-1"
                                 >
                                     <p className="text-center">{task.desc}</p>
                                     <button className="btn btn-outline-danger">Borrar</button>
@@ -36,7 +53,9 @@ export const TaskApp = () => {
                 <div className="col-5">
                     <h3>Agregar Task</h3>
                     <hr/>
-                    <form>
+                    <form
+                        onSubmit={handleAddTask}
+                    >
                         <input
                             type="text"
                             name="description"
@@ -45,6 +64,7 @@ export const TaskApp = () => {
                             autoComplete="off"
                         />
                         <button
+                            type="submit"
                             className="btn btn-outline-success mt-2 btn-block"
                         >
                             Agregar
--- src/components/08-useReducer/taskReducer.js
+++ src/components/08-useReducer/taskReducer.js
@@ -1,6 +1,6 @@
 export const taskReducer = ( state = [], action ) => {
     switch(action.type){
-
+        case 'add': return [...state, action.payload];
 
         default: return state;
     }
