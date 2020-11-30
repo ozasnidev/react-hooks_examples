5446da1a0b4c4468b08708f42abbf4b3725f868e
Ozasnidev <ozasnidev@gmail.com>
2020-11-30 17:55:26 +0100

    16 - Testing: UseCouter hook functions test

--- src/test/hooks/useCouter.test.js
+++ src/test/hooks/useCouter.test.js
@@ -1,7 +1,7 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useCouter } from '../../hooks/useCouter';
 
-describe('useConter hook test', () => {
+describe('useCouter hook test', () => {
     test('return initial values', () => {
         const { result } = renderHook(() => useCouter());
         expect(result.current.counter).toBe(10);
@@ -9,4 +9,18 @@ describe('useConter hook test', () => {
         expect(typeof result.current.decrement).toBe('function');
         expect(typeof result.current.reset).toBe('function');
     }); 
+    test('increment counter', () => {
+        const { result } = renderHook(() => useCouter());
+        const { increment } = result.current;
+        act( () => increment());
+        const { counter } = result.current; 
+        expect( counter ).toBe(11);
+    });
+    test('decrement & reset counter', () => {
+        const { result } = renderHook(() => useCouter());
+        const { decrement, reset } = result.current;
+        act( () => {decrement(); reset()});
+        const { counter } = result.current; 
+        expect( counter ).toBe(10);
+    });
 });
