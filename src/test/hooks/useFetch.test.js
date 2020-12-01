d41ceb25a5c862e82d18e5fd7cdcb5c595904968
Ozasnidev <ozasnidev@gmail.com>
2020-12-01 22:51:40 +0100

    19 - Testing: useFetch hook. return a Pokemon as data and sucessfull request test

--- src/test/hooks/useFetch.test.js
+++ src/test/hooks/useFetch.test.js
@@ -10,4 +10,14 @@ describe('useFetch hook test', () => {
         expect(loading).toBe(true);
         expect(error).toBe(null);
     });
+    test('return a Pokemon as data and sucessfull request', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetch('https://pokeapi.co/api/v2/pokemon/ho-oh'));
+        await waitForNextUpdate();
+        const { data, loading, error } = result.current;
+
+        expect(data.name).toBe('ho-oh');
+        expect(loading).toBeFalsy();
+        expect(error).toBe(null);
+    })
+    
 });
